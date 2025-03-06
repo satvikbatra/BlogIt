be4486985fad6611ea1fe5e21605389321bc918d
Satvik Batra <118475547+satvikbatra@users.noreply.github.com>
2025-03-06 18:45:24 +0530

    fixed a bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,11 @@ import { Blogs } from './pages/blogs'
 import { Publish } from './pages/publish'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Signup />}></Route>
           <Route path='/signup' element={<Signup />}></Route>
           <Route path='/signin' element={<Signin />}></Route>
           <Route path='/blog/:id' element={<Blog />}></Route>
